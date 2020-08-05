7a0dc51f0a3c681463ed5415bfd51dbfadacfc47
Pobx <geidtiphong.s@allrightbox.com>
2020-08-05 15:49:18 +0700

    test add new Order OK

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  ValidationPipe,
+  ParseIntPipe,
+  Param,
+  Put,
+  Body,
+  Post,
+  Get,
+  Delete,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrderDto } from './orders.dto';
+import { Order } from './order.entity';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private catsService: OrdersService) {}
+
+  @Post()
+  async create(@Body(new ValidationPipe()) orderDto: OrderDto): Promise<Order> {
+    return this.catsService.create(orderDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Order[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.catsService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id,
+    @Body(new ValidationPipe()) orderDto: OrderDto,
+  ): Promise<any> {
+    return this.catsService.update(id, orderDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id): Promise<any> {
+    return this.catsService.delete(id);
+  }
+}
--- src/orders/orders.dto.ts
+++ src/orders/orders.dto.ts
@@ -2,6 +2,5 @@ import { IsInt } from 'class-validator';
 export class OrderDto {
   @IsInt()
   Id: number;
-  createdDate: any;
   isActive?: boolean;
 }
