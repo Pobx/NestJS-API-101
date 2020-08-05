072e6bcf13d7bf2274395e4d4b9e5bc0cd59d7ed
Pobx <geidtiphong.s@allrightbox.com>
2020-08-05 11:20:17 +0700

    export and import CatsModule and try to use CatsService on fishController

--- src/fish/fish.controller.ts
+++ src/fish/fish.controller.ts
@@ -1,7 +1,16 @@
 import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { CatsService } from 'src/cats/cats.service';
+import { Cat } from 'src/cats/cat.entity';
 
 @Controller('fish')
 export class FishController {
+  constructor(private catsService: CatsService) {}
+
+  @Get('cats')
+  async findCatAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
   @Get()
   findAll(): string {
     return `Hello from FishController`;
--- src/fish/fish.module.ts
+++ src/fish/fish.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FishController } from './fish.controller';
+import { CatsModule } from 'src/cats/cats.module';
+import { CatsService } from 'src/cats/cats.service';
 
 @Module({
   controllers: [FishController],
+  imports: [CatsModule],
+  providers: [CatsService],
 })
 export class FishModule {}
