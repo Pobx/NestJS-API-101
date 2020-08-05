6def40f79e36b8fa49b6f3bd3383713b1b8d40df
Pobx <geidtiphong.s@allrightbox.com>
2020-08-05 15:38:37 +0700

    add Repository to OrdersService

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './order.entity';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import { OrderDto } from './orders.dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(@InjectRepository(Order) private repository: Repository<Order>) {}
+
+  async findAll(): Promise<Order[]> {
+    return await this.repository.find();
+  }
+
+  async findOne(id: number): Promise<Order> {
+    return await this.repository.findOne(id);
+  }
+
+  async create(cat: OrderDto): Promise<Order> {
+    return await this.repository.save(cat);
+  }
+
+  async update(id: number, cat: OrderDto): Promise<UpdateResult> {
+    return await this.repository.update(id, cat);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.repository.delete(id);
+  }
+}
