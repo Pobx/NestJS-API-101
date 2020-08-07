f802ca59bf6084dbfca244a3b3ef6a34f40aaac3
Pobx <geidtiphong@gmail.com>
2020-08-07 18:13:46 +0700

    OrdersModule and ItemsModule to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,13 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './logger.middleware';
-import { DogsModule } from './dogs/dogs.module';
-import { FishModule } from './fish/fish.module';
-import { CatsController } from './cats/cats.controller';
-import { FishController } from './fish/fish.controller';
-import { DogsController } from './dogs/dogs.controller';
 import { Connection } from 'typeorm';
 import { OrdersModule } from './orders/orders.module';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    CatsModule,
-    CatsModule,
-    DogsModule,
-    FishModule,
-    OrdersModule,
-    ItemsModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), OrdersModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
@@ -30,8 +16,6 @@ export class AppModule implements NestModule {
   constructor(private readonly connection: Connection) {}
 
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes(CatsController, FishController, DogsController);
+    consumer.apply(LoggerMiddleware).forRoutes(OrdersModule, ItemsModule);
   }
 }
